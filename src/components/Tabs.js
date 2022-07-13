51d89138f25c9ca9645aace66b3c1f267ae4dff8
SatyamAK <satyam.ak99@gmail.com>
2022-07-13 17:44:06 +0530

    Tabs finished

---
+++ src/components/Tabs.js
@@ -0,0 +1,12 @@
+import './Tabs.css';
+
+export default function Tabs(){
+
+    return (
+        <div className='Tabs'>
+           <div className='Tab-Active'>All</div>
+           <div className='Tab'>Active</div>
+           <div className='Tab'>Completed</div> 
+        </div>
+    );
+}
--- src/pages/Todo.js
+++ src/pages/Todo.js
@@ -1,11 +1,13 @@
 import Heading from "../components/Heading";
 import ToDoForm from "../components/ToDoForm";
+import Tabs from "../components/Tabs";
 
 export default function Todo(){
     return(
         <div className="toDo">
             <Heading heading = "ToDoMatic" />
             <ToDoForm />
+            <Tabs />
         </div>
     );
 }
